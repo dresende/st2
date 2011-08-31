477e407c77a51a937efa052609683808350e19c8
Diogo Resende <dresende@thinkdigital.pt>
2011-08-31 16:59:05 +0100

    changed diff command to be only 1 class

---
+++ diff.py
@@ -0,0 +1,31 @@
+import sublime, sublime_plugin
+import subprocess
+
+diff_files = []
+
+class DiffAddFileCommand(sublime_plugin.WindowCommand):
+	def run(self):
+		global diff_files
+
+		if len(diff_files) > 1:
+			diff_files = diff_files[-1:]
+		diff_files.append(self.window.active_view().file_name())
+
+		print diff_files
+
+		if len(diff_files) == 2:
+			p = subprocess.Popen(["diff", diff_files[0], diff_files[1]], bufsize=4096, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+			stdout, stderr = p.communicate()
+
+			output = stdout
+			if stderr != "":
+				output = stderr
+
+			diff_view = self.window.new_file()
+			diff_view.set_scratch(True)
+
+			diff_edit = diff_view.begin_edit()
+			diff_view.insert(diff_edit, diff_view.size(), output)
+			diff_view.end_edit(diff_edit)
+
+			diff_view.set_syntax_file("Packages/Diff/Diff.tmLanguage")
