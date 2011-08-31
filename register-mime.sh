ac4b8b3253b979e46274a16a5e6819635888c71e
Diogo Resende <dresende@thinkdigital.pt>
2011-08-31 16:59:58 +0100

    added command to register sublime mime and icons (Gnome)

---
+++ register-mime.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+FOUND=`grep 'application/x-extension-sublime-project' /etc/mime.types`
+if [ -z "$FOUND" ]; then
+	echo "Registering MIME.."
+	echo 'application/x-extension-sublime-project              sublime-project' >> /etc/mime.types
+	mkdir -p /usr/share/icons/gnome/scalable/mimetypes/
+	ln -s `pwd`/sublime-project-mime-icon.svg /usr/share/icons/gnome/scalable/mimetypes/application-x-extension-sublime-project.svg
+	ln -s `pwd`/sublime-project-mime-icon.png /usr/share/icons/gnome/48x48/mimetypes/application-x-extension-sublime-project.png
+else
+	echo "MIME already registed"
+fi
